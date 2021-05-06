61a4a1b4d4a5b29f85f4b2de577b17ef121ab812
shangar <sharanshangar@gmail.com>
2021-05-06 00:26:05 -0400

    gets occupancy mask for rook

--- board.cpp
+++ board.cpp
@@ -23,10 +23,44 @@ class Board {
 
   }
 
-  std::vector<Move> getMovesByPiece(std::vector<Move> &moves, PieceType pieceType) {
-    switch (pieceType) {
-
-    }
-  	// Assume you are not in check
+  U64 get_occupancy(U64 location, PieceType piece){
+  	// function takes in piece location in bitboard format and returns all possible moves across board
+  	U64 mask;
+  	switch(piece){
+  		case NONE:
+  			break;
+  		case Pawn:
+  			break; 
+  		case Rook:
+  			mask = 0; 
+  			//vertical above piece
+  			for (int i = location+8; i<= 63-8; i+=8){
+  				mask |= (((U64)1)<<i);
+  			}
+  			//vertical below piece
+  			for (int i = location-8; i>= 8; i-=8){
+  				mask |= (((U64)1)<<i);
+  			}
+  			//horizontal to the right
+  			for (int i = location + 1; i%8<=7 && i%8!=0; i++){
+  				mask |= (((U64)1)<<i)
+  			}
+  			//horizontal to the left
+  			for (int i = location - 1; i%8<=7 && i&8!=0; i--){
+  				mask |= (((U64)1)<<i)
+  			}
+  			break
+  		case Knight:
+  			break;
+  		case Bishop:
+  			break; 
+  		case Queen:
+  			break;
+  		case KingL
+  			break;
+  	}
+  	return mask;
   }
+
+
 };
