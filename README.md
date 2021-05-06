6437dd44e97b65b7730a17f2ec4396cb7993bd67
Sam Prokopchuk <ssprokopchuk@gmail.com>
2021-05-05 23:15:17 -0400

    Use advice from chessprogramming.org

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # chess_engine
+
+## Move Generation:
+ 1. If King is in check find all legal moves.
+ 	1a. If no king moves -> is checkmate.
+ 2. Find pinned pieces.
+ 3. Find all legal moves for non-pinned pieces.
+ 
--- board.cpp
+++ board.cpp
@@ -1,59 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <vector>
+#include "types.h"
 
-enum PieceType { Pawn, Rook, Knight, Bishop, Queen, King };
-
-struct Piece {
-  PieceType type;
-  char row, col;
-  int lastMove;
-};
+enum PieceType { NONE, Pawn, Rook, Knight, Bishop, Queen, King };
 
 struct Move {
-  char prevRow, prevCol, nextRow, nextCol
+  char prev, next;
 };
 
 class Board {
  public:
-  Piece *board[8][8];
-  std::vector<Piece> whitePieces;
-  std::vector<Piece> blackPieces;
+  PieceType whitePieces[64] = { NONE };
+  PieceType blackPieces[64] = { NONE };
+  U64 whitePiecesIndexes;
+  U64 blackPiecesIndexes;
+  U64 bitboard;
 
   Board() {
-    for (char col = 0; col < 8; ++col) {
-      whitePieces.push_back({Pawn, 6, col, 0});
-      blackPieces.push_back({Pawn, 1, col, 0});
-    }
-    whitePieces.push_back({Rook, 7, 0, 0});
-    whitePieces.push_back({Knight, 7, 1, 0});
-    whitePieces.push_back({Bishop, 7, 2, 0});
-    whitePieces.push_back({Queen, 7, 3, 0});
-    whitePieces.push_back({King, 7, 4, 0});
-    whitePieces.push_back({Bishop, 7, 5, 0});
-    whitePieces.push_back({Knight, 7, 6, 0});
-    whitePieces.push_back({Rook, 7, 7, 0});
-    blackPieces.push_back({Rook, 0, 0, 0});
-    blackPieces.push_back({Knight, 0, 1, 0});
-    blackPieces.push_back({Bishop, 0, 2, 0});
-    blackPieces.push_back({Queen, 0, 3, 0});
-    blackPieces.push_back({King, 0, 4, 0});
-    blackPieces.push_back({Bishop, 0, 5, 0});
-    blackPieces.push_back({Knight, 0, 6, 0});
-    blackPieces.push_back({Rook, 0, 7, 0});
+    whitePieces = 18446462598732840960;
+    blackPieces = 65535;
 
-    std::fill(begin(board), end(board), nullptr);
-
-    for (Piece &p : whitePieces) {
-      board[p.row][p.col] = &p;
-    }
-    for (Piece &p : blackPieces) {
-      board[p.row][p.col] = &p;
-    }
   }
 
-  std::vector<Move> getPawnMoves(std::vector<Move> &moves) {
+  std::vector<Move> getMovesByPiece(std::vector<Move> &moves, PieceType pieceType) {
+    switch (pieceType) {
+
+    }
   	// Assume you are not in check
   }
 };
