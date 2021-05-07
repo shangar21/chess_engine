20af4c2f47047286c0443fe3da04f0eaf0488e5f
Sam Prokopchuk <ssprokopchuk@gmail.com>
2021-05-06 22:40:25 -0400

    Minor changes

--- board.cpp
+++ board.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <vector>
+
 #include "types.h"
 
 enum PieceType { NONE, Pawn, Rook, Knight, Bishop, Queen, King };
@@ -11,45 +13,42 @@ struct Move {
 
 class Board {
  public:
-  PieceType whitePieces[64] = { NONE }; // U64 whitePieces = 0;
-  PieceType blackPieces[64] = { NONE }; // U64 whitePieces = 0;
+  PieceType whitePieces[64] = {NONE};  // U64 whitePieces = 0;
+  PieceType blackPieces[64] = {NONE};  // U64 whitePieces = 0;
   U64 whitePiecesIndexes;
   U64 blackPiecesIndexes;
   U64 bitboard;
 
   Board() {
-    whitePieces = 18446462598732840960;
-    blackPieces = 65535;
-
+    whitePiecesIndexes = 18446462598732840960U;
+    blackPiecesIndexes = 65535U;
   }
 
-  U64 get_rook_occupancy(U64 location, PieceType piece){
-  		mask = 0; 
-  			//vertical above piece
-  		for (int i = location+8; i<= 63-8; i+=8){
-  			mask |= (((U64)1)<<i);
-  		}
-		//vertical below piece
-		for (int i = location-8; i>= 8; i-=8){
-			mask |= (((U64)1)<<i);
-		}
-		//horizontal to the right
-		for (int i = location + 1; i%8<=7 && i%8!=0; i++){
-			mask |= (((U64)1)<<i)
-		}
-		//horizontal to the left
-		for (int i = location - 1; i%8<=7 && i&8!=0; i--){
-			mask |= (((U64)1)<<i)
-		}
-		return mask 
+  U64 get_rook_occupancy(U64 location, PieceType piece) {
+    U64 mask = 0;
+    // vertical above piece
+    for (int i = location + 8; i <= 63 - 8; i += 8) {
+      mask |= (((U64)1) << i);
+    }
+    // vertical below piece
+    for (int i = location - 8; i >= 8; i -= 8) {
+      mask |= (((U64)1) << i);
+    }
+    // horizontal to the right
+    for (int i = location + 1; i % 8 <= 7 && i % 8 != 0; i++) {
+      mask |= (((U64)1) << i);
+    }
+    // horizontal to the left
+    for (int i = location - 1; i % 8 <= 7 && i & 8 != 0; i--) {
+      mask |= (((U64)1) << i);
+    }
+    return mask;
   }
 
-  U64 get_bishop_occupancy(U64 location, PieceType piece){
-  	return 0 
-  }
+  U64 get_bishop_occupancy(U64 location, PieceType piece) { return 0; }
 
-  U64 get_queen_occupancy(U64 location, PieceType piece){
-  	return get_rook_occupancy(location, piece) | get_bishop_occupancy(location, piece)
+  U64 get_queen_occupancy(U64 location, PieceType piece) {
+    return get_rook_occupancy(location, piece) |
+           get_bishop_occupancy(location, piece);
   }
-
 };
