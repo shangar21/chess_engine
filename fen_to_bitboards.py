f3e71409e3753ce91fb046515c106adacaf6e167
shangar <sharanshangar@gmail.com>
2021-08-06 23:37:13 -0400

    asdhjkfaklsjfd

--- fen_to_bitboards.py
+++ fen_to_bitboards.py
@@ -1,6 +1,8 @@
 #!/bin/python
 
 import chess
+import random
+import json
 
 from sys import argv
 
@@ -13,9 +15,35 @@ def printBitBoards(bb: chess.BaseBoard, color: chess.Color):
     print(f'0x{int(bb.pieces(chess.QUEEN, color)):0{16}x},')
     print(f'0x{int(bb.pieces(chess.KING, color)):0{16}x}}}')
 
+def bitBoardToDict(bb:chess.BaseBoard, color: chess.Color):
+    return {"pawn": int(bb.pieces(chess.PAWN, color)),
+            "rook": int(bb.pieces(chess.ROOK, color)),
+            "knight": int(bb.pieces(chess.KNIGHT, color)),
+            "bishop": int(bb.pieces(chess.BISHOP, color)),
+            "queen": int(bb.pieces(chess.QUEEN, color)),
+            "king":int(bb.pieces(chess.KING, color))
+            }
 
-baseBoard = chess.BaseBoard(board_fen=argv[1])
+
+res = argv[1].split()
+
+baseBoard = chess.BaseBoard(board_fen=res[0])
+
 print('WHITE BITBOARDS:')
 printBitBoards(baseBoard, chess.WHITE)
 print('BLACK BITBOARDS:')
 printBitBoards(baseBoard, chess.BLACK)
+
+white_pieces = bitBoardToDict(baseBoard, chess.WHITE)
+black_pieces = bitBoardToDict(baseBoard, chess.BLACK)
+
+all = {argv[1]: {"white_pieces" : white_pieces, "black_pieces": black_pieces, 'white': random.getrandbits(1)}}
+
+with open("tests.json", "r+") as out:
+    try:
+        f = json.load(out)
+        f.update(all)
+        json.dump(f, out, indent=4)
+    except json.decoder.JSONDecodeError as e:
+        json.dump(all, out, indent=4)
+
